45ba9e11fd3f50d722d1b7a95329d0b349cc8285
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-09-03 21:28:25 +0900

    Update config'

--- website/config/application.rb
+++ website/config/application.rb
@@ -23,10 +23,6 @@ module Web
     config.load_defaults 5.2
 
 
-    config.api_only = true
-
-    config.debug_exception_response_format = :api
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
