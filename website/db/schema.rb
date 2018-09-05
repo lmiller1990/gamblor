be74f4e614e9b31dccf1ae1e06e6fd85b616cc0d
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-09-06 00:10:35 +0900

    Add player table

--- website/db/schema.rb
+++ website/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_145048) do
+ActiveRecord::Schema.define(version: 2018_09_05_150924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2018_09_03_145048) do
     t.integer "blue_side_team_id", null: false
     t.integer "first_baron_team_id"
     t.integer "first_dragon_team_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teams", force: :cascade do |t|
--- website/spec/rails_helper.rb
+++ website/spec/rails_helper.rb
@@ -41,6 +41,14 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
